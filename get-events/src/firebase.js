04f523e739d9630a226a286ecdb45da370b0f26f
Wesley Kelly <wesley.kelly@hotmail.com>
2022-03-25 15:07:52 +0000

    firebase

--- get-events/src/firebase.js
+++ get-events/src/firebase.js
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
+//import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
