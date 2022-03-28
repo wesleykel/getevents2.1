9deac5f18d22c34c40fd668bd41838ed32f42a19
Wesley Kelly <wesley.kelly@hotmail.com>
2022-03-28 14:04:41 +0100

    auth 0 added and fav route added

--- get-events/src/components/4Navbar/index.js
+++ get-events/src/components/4Navbar/index.js
@@ -1,8 +1,8 @@
 import style from "./navbar.module.css"
-//import  { Link } from "react-router-dom"
+
 import React from 'react'
 import { useAuth0 } from "@auth0/auth0-react";
-import Favourites from "../../routes/favourites";
+
 
 
 
